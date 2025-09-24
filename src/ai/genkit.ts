b1105c909842addd253508daf70c1cd030eea4a1
Nunu Giuseppin <nunugiuseppin@gmail.com>
2025-09-24 13:23:39 +0000

    Error: [ Server ] Failed to get news summary: {}     at createConsoleErr

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,13 @@
-import {genkit} from 'genkit';
+import {genkit, Plugin} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
+let googleAiPlugin: Plugin<any> | undefined = undefined;
+if (process.env.GEMINI_API_KEY) {
+  googleAiPlugin = googleAI({
+    apiKey: process.env.GEMINI_API_KEY,
+  });
+}
+
 export const ai = genkit({
-  plugins: [
-    googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
-    }),
-  ],
+  plugins: [googleAiPlugin].filter((p): p is Plugin<any> => !!p),
 });
