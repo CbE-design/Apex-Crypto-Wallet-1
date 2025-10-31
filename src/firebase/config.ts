d5e4e01f1418caf928891d715e80321c783cfc16
Nunu Giuseppin <nunugiuseppin@gmail.com>
2025-10-31 10:30:58 +0000

    import { initializeApp } from "firebase/app"; import { getFirestore } fr

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "studio-8025635453-a4860",
   "appId": "1:8096780200:web:a5eb539594b5608314979d",
+  "storageBucket": "studio-8025635453-a4860.appspot.com",
   "apiKey": "AIzaSyDS9a7kdaxG0bnk0SGbszoRQPJgJL29gu8",
   "authDomain": "studio-8025635453-a4860.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "8096780200"
 };
