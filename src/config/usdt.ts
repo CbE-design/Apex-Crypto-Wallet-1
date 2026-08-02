d5611434207a3f4bb910016e9c6eb521c81b5e64
Luis Watson <dirkievanderwesthuizen931@gmail.com>
2026-08-02 23:11:28 +0200

    Add USDT contract address and ABI definitions

---
+++ src/config/usdt.ts
@@ -0,0 +1,21 @@
+export const USDT_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7' as const;
+
+export const USDT_ABI = [
+  {
+    name: 'balanceOf',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [{ name: 'account', type: 'address' }],
+    outputs: [{ name: '', type: 'uint256' }],
+  },
+  {
+    name: 'transfer',
+    type: 'function',
+    stateMutability: 'nonpayable',
+    inputs: [
+      { name: 'to', type: 'address' },
+      { name: 'value', type: 'uint256' },
+    ],
+    outputs: [{ name: '', type: 'bool' }],
+  },
+] as const;
