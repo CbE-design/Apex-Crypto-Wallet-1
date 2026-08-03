4b867fd493bc21557b7524feb053bc41c9df4fb3
Luis Watson <dirkievanderwesthuizen931@gmail.com>
2026-08-03 01:33:36 +0000

    Revert "Add debug script for initialization logging"
    This reverts commit c92d96803bf16664a1f948b2ebf2953a91f08f37.

--- src/scripts/debug.ts
+++
@@ -1,7 +0,0 @@
-console.log('1. Script starting...');
-
-import { USDT_ADDRESS } from '../config/usdt';
-console.log('2. Config loaded:', USDT_ADDRESS);
-
-import { publicClient } from '../lib/seedUsdt';
-console.log('3. Seed lib loaded successfully');
